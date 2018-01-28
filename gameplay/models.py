d2c99de55457d954ed5641d2ab233ad9f599fb4e
Andrew Gulik <andrewgulik@gmail.com>
2018-01-28 14:10:05 -0500

    first commit

---
+++ gameplay/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Game (models.Model):
+    first_player = models.ForeignKey(User, related_name="games_first_player")
+    second_player = models.ForeignKey(User, related_name="games_second_player")
+    start_time = models.DateTimeField(auto_now_add=True)
+    last_active = models.DateTimeField(auto_now=True)
+
+class Move (models.Model):
+    x = models.IntegerField()
+    y = models.IntegerField()
+    comment = models.CharField(max_length=300, blank=True)
+    by_first_player = models.BooleanFied()
+
+        # add relation from moves to games
+        game=models.ForeignKey(Game, on_delete=models.CASCADE) # if the game gets deleted, so do all the moves that are linked to the game
